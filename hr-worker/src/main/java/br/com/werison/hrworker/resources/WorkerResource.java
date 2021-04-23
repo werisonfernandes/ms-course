3f4ec3f9943b6757e67027b47598a15f695c881d
Werison Aparecido Fernandes <waparecf@everis.com>
2021-04-23 15:26:21 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/br/com/werison/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/com/werison/hrworker/resources/WorkerResource.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +22,9 @@ import br.com.werison.hrworker.repositories.WorkerRepository;
 public class WorkerResource {
 	
 	private Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Value("${test.config}")
+	private String testConfig;
 	
 	@Autowired
 	private Environment env;
@@ -50,6 +54,13 @@ public class WorkerResource {
 		Optional<Worker> worker = repository.findById(id); //repository.findById(id).get();
 		
 		return ResponseEntity.ok(worker);
+	}
+	
+	@GetMapping(value="/configs")
+	public ResponseEntity<Void> getConfig() {
+		logger.info("CONFIG => " + testConfig);
+		
+		return ResponseEntity.noContent().build();
 	}
 
 }
