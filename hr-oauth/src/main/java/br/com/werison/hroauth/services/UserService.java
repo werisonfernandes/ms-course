4eb1bec9010bbcf1713a0a8b9d737be397751a55
Werison Aparecido Fernandes <waparecf@everis.com>
2021-04-24 11:43:43 -0300

    Login, JWT Authorization part-2

--- hr-oauth/src/main/java/br/com/werison/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/br/com/werison/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package br.com.werison.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import br.com.werison.hroauth.entities.User;
 import br.com.werison.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
 	Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -25,6 +28,19 @@ public class UserService {
 		}
 
 		logger.info("Email found " + email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+
+		if (user == null) {
+			logger.error("Email not found!");
+			throw new UsernameNotFoundException("Email not found!");
+		}
+
+		logger.info("Email found " + username);
 		return user;
 	}
 }
